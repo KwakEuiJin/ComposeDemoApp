353f1ddb695232f0bfaa8d1e5d844f7ae84dab0f
곽의진 <yuijin0909@gmail.com>
2023-02-21 21:35:06 +0900

    이미지 오류 수정

--- app/src/main/java/com/timertiti/composedemoapp/MainActivity.kt
+++ app/src/main/java/com/timertiti/composedemoapp/MainActivity.kt
@@ -3,11 +3,7 @@ package com.timertiti.composedemoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
@@ -18,7 +14,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberImagePainter
+import coil.compose.AsyncImage
 import com.timertiti.composedemoapp.ui.theme.ComposeDemoAppTheme
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -58,10 +54,11 @@ fun Item(itemData: ItemData) {
         Column(
             modifier = Modifier
                 .padding(8.dp)
-                .fillMaxSize()
+                .fillMaxWidth()
         ) {
-            Image(
-                painter = rememberImagePainter(data = "https://w.namu.la/s/0f0f182dcbb699f9b1d7d2e026b8654dbc881bfacf21427fe20dca1c0557bd38ffeb0c108c070906773b0e3f16eaf85ca44ab8481804761662c5eaa6b459465311c500c760eb5e8644c850d31c83ccd04305661e65b35b2522be38ec78b3307ef59a9ea9fcc8bcebf0184132cb372458"),
+            AsyncImage(
+                model = itemData.imageId ,
+                modifier =  Modifier.fillMaxWidth(),
                 contentDescription = itemData.title,
             )
             Spacer(modifier = Modifier.padding(8.dp))
@@ -88,8 +85,12 @@ fun CatalogEx(itemList: List<ItemData>) {
 
 @Composable
 fun Greeting(){
-    val painter = rememberImagePainter(data = "https://w.namu.la/s/0f0f182dcbb699f9b1d7d2e026b8654dbc881bfacf21427fe20dca1c0557bd38ffeb0c108c070906773b0e3f16eaf85ca44ab8481804761662c5eaa6b459465311c500c760eb5e8644c850d31c83ccd04305661e65b35b2522be38ec78b3307ef59a9ea9fcc8bcebf0184132cb372458")
-    Image(painter = painter, contentDescription = "")
+    LazyColumn{
+        item {
+            AsyncImage(model = "https://w.namu.la/s/0f0f182dcbb699f9b1d7d2e026b8654dbc881bfacf21427fe20dca1c0557bd38ffeb0c108c070906773b0e3f16eaf85ca44ab8481804761662c5eaa6b459465311c500c760eb5e8644c850d31c83ccd04305661e65b35b2522be38ec78b3307ef59a9ea9fcc8bcebf0184132cb372458", contentDescription = "")
+        }
+    }
+
 }
 
 @Preview(showBackground = true)

647b2098d5cc3366418f4f93c62a7d54070c6303
곽의진 <yuijin0909@gmail.com>
2023-02-26 15:32:13 +0900

    Pager 추가

--- app/src/main/java/com/kejprogramer/composedemoapp/MainActivity.kt
+++ app/src/main/java/com/kejprogramer/composedemoapp/MainActivity.kt
@@ -14,6 +14,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.paging.PagingState
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
 import com.kejprogramer.composedemoapp.ui.theme.ComposeDemoAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,8 +34,15 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun BottomNavigationScreens() {
+    val pagerState = rememberPagerState()
+    HorizontalPager( // 가로로 스크롤 가능한 pager
+        count = 5, // 페이지 수
+        state = pagerState,
+        content = {}// PagerState
+    )
     val currentTabIndex = remember { mutableStateOf(0) }
     Scaffold(
         bottomBar = {
@@ -60,14 +71,38 @@ fun BottomNavigationScreens() {
             .fillMaxSize()
             .padding(innerPadding)
 
-        val screens: List<@Composable (Modifier) -> Unit> = listOf(
-            { Screen1() },
-            { Screen2() }
-        )
-        screens[currentTabIndex.value].invoke(modifier)
+        val pagerState = rememberPagerState()
+        HorizontalPager( // 가로로 스크롤 가능한 pager
+            count = 2, // 페이지 수
+            state = pagerState
+        ) { page ->
+            when (page) {
+                0 -> Screen1()
+                1 -> Screen2()
+
+            }
+        }
     }
 }
 
+class PageDataSource(private val screens: List<@Composable (Modifier) -> Unit>) :
+    androidx.paging.PagingSource<Int, Int>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Int> {
+        val currentPage = params.key ?: 0
+        val nextIndex = currentPage + 1
+        val prevIndex = if (currentPage > 0) currentPage - 1 else null
+        return LoadResult.Page(
+            data = listOf(currentPage),
+            prevKey = prevIndex,
+            nextKey = nextIndex
+        )
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Int>): Int? {
+        return state.anchorPosition
+    }
+}
 
 @Composable
 fun Screen1(modifier: Modifier = Modifier) {
